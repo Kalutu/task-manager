331c5e131be0ae2f52bc9c5a176a4903f287bc87
Kalutu Daniel <kalutudaniel@gmail.com>
2023-05-21 07:26:11 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+    $("#input").change(function() {
+      let input = $(this).val();
+      $("ul").append('<li>' + input + '<i class="fas fa-trash"></i><i class="fas fa-check"></i></li>');
+      $(this).val("");
+    });
+  
+    $("ul").on("click", ".fa-check", function() {
+      $(this).parent("li").toggleClass("checked");
+    });
+  
+    $("ul").on("click", ".fa-trash", function() {
+      $(this).parent("li").fadeOut(200);
+    });
+  });
+  

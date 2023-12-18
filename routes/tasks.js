d2b9441c89c58dcdba915cd87f7c2d3acf338317
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-18 17:36:12 +0300

    Express router

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  editTask,
 } = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)

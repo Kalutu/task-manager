4cfdeef23d3fba361bdd6de87c9b120c2f5e080e
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-18 17:32:54 +0300

    Updated routes

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-Found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
+//middleware
 app.use(express.static('./public'))
 app.use(express.json())
 
 //routes
 app.use('/api/v1/tasks', tasks)
-
-
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = 3000
 

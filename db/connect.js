1f32ace9e33d54b5e932b2f6934cf619ac7dff39
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-18 17:34:15 +0300

    Handled deprecation warnings

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 
-
-const connectDB = (url)=>{
- return mongoose
-            .connect(url,{
-                useNewUrlParser: true,
-                useCreateIndex: true,
-                useFindAndModify: false,
-                useUnifiedTopology: true
- })
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
 }
 
 module.exports = connectDB

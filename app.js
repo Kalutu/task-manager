3cf079ed750c902cd46f946d6d1854e749184867
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-19 15:25:18 +0300

    Change port value to be flexible

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async()=>{
     try {
